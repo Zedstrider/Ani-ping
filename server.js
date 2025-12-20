d8185dfabcaaa0bf12add88afafbbf2ca8df5004
Zedstrider <206158702+Zedstrider@users.noreply.github.com.>
2025-12-20 16:02:35 +0530

    add home route

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-const express=require('express')
+const express=require('express') //include external modules
 const app=express()
+
+const PORT = 5501
+
+app.listen(PORT,()=>{console.log(`Server is running on port ${PORT}`)})
+
+app.get('/',(req,res)=>{
+    res.send(`<p>Ani-Ping is online!</p>`)
+})
