1dcd5bcca80fd3d06aa9d427c163895fbb79243b
Zedstrider <206158702+Zedstrider@users.noreply.github.com.>
2025-12-21 15:06:51 +0530

    secure credentials and finalize app for deployment

--- server.js
+++ server.js
@@ -1,7 +1,58 @@
 const express=require('express') //include external modules
 const app=express()
+const nodemailer = require('nodemailer')
+const axios=require('axios')
+require('dotenv').config()
 
 const PORT = 5501
+
+let lastSentAnime
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS
+  }
+})
+
+setInterval(async ()=>{
+    try{
+        const response = await axios.get('https://api.jikan.moe/v4/schedules')
+         //list of anime objects
+        const animeList = response.data.data;
+
+        const targetAnime=animeList.find((anime)=>{
+            return anime.title==='One Piece'
+        })
+        
+        //Debug Logs
+        console.log("--------------------------------");
+        console.log("Check run at:", new Date().toLocaleTimeString());
+        console.log("Did we find the show?", targetAnime ? "YES" : "NO");
+        if (targetAnime) console.log("Memory says:", lastSentAnime);
+        //-----------------------------------
+        if(targetAnime&&lastSentAnime!=targetAnime.title){
+            const mailOptions = {
+                from: process.env.GMAIL_USER,
+                to: process.env.GMAIL_USER, 
+                subject: `New episode alert:${targetAnime.title}`,
+                text: `Heads up! ${targetAnime.title} is currently airing.`
+            }
+        
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log('Error:', error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    lastSentAnime=targetAnime.title
+                }
+            })
+        }
+    }catch(error){
+        console.error("Something went wrong:",error)
+    }
+},3600000)
 
 app.listen(PORT,()=>{console.log(`Server is running on port ${PORT}`)})
 
